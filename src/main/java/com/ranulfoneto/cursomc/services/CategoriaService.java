b2ca5515b0a0a5fce35101457b177f2ea110d671
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-07-05 08:48:47 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/ranulfoneto/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.ranulfoneto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ranulfoneto.cursomc.domain.Categoria;
+import com.ranulfoneto.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
