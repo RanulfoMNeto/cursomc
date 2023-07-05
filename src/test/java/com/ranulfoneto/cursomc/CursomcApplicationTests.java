6bd637eb584ef98293754edbab97e3a5801e7acc
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-07-05 03:55:06 -0300

    Projeto criado

---
+++ src/test/java/com/ranulfoneto/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranulfoneto.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
