871f511f30cc4c8548698f9bf53ebfa4ba66a5b4
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-07-06 02:36:41 -0300

    Criando operação de instanciação

--- src/main/java/com/ranulfoneto/cursomc/CursomcApplication.java
+++ src/main/java/com/ranulfoneto/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.ranulfoneto.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ranulfoneto.cursomc.domain.Categoria;
+import com.ranulfoneto.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
