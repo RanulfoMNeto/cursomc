e204bf0a3bb9d74aca40f6aa6dac7eecbb28bd63
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-07-11 22:57:50 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/ranulfoneto/cursomc/services/CategoriaService.java
+++ src/main/java/com/ranulfoneto/cursomc/services/CategoriaService.java
@@ -24,4 +24,9 @@ public class CategoriaService {
 		obj.setId(null);
         return repo.save(obj);
     }
+
+    public Categoria update(Categoria obj) throws ObjectNotFoundException {
+		find(obj.getId());
+		return repo.save(obj);
+    }
 }
