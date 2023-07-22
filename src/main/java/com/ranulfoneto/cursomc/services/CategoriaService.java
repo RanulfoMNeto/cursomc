debfa04a4a9ee48ce6dd83d2d9acbf58c86ec100
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-07-22 06:12:40 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/ranulfoneto/cursomc/services/CategoriaService.java
+++ src/main/java/com/ranulfoneto/cursomc/services/CategoriaService.java
@@ -33,8 +33,9 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria obj) throws ObjectNotFoundException {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
     }
 
 	public void delete(Integer id) throws ObjectNotFoundException, DataIntegrityException {
@@ -57,5 +58,9 @@ public class CategoriaService {
 
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
