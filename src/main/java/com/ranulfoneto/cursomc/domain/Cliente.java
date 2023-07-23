a853cf356f406d457b4efefe3b12c68c47fd9963
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-07-23 15:54:36 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/ranulfoneto/cursomc/domain/Cliente.java
+++ src/main/java/com/ranulfoneto/cursomc/domain/Cliente.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ranulfoneto.cursomc.domain.enums.TipoCliente;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
@@ -30,7 +31,7 @@ public class Cliente implements Serializable {
     private String cpfOuCnpj;
     private Integer tipo;
 
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection
--- src/main/java/com/ranulfoneto/cursomc/services/ClienteService.java
+++ src/main/java/com/ranulfoneto/cursomc/services/ClienteService.java
@@ -55,7 +55,7 @@ public class ClienteService {
         try {
             repo.deleteById(id);
         } catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityException("Não é possível porque há entidades relacionadas");
+            throw new DataIntegrityException("Não é possível porque há produtos relacionadas");
         }
     }
     
