a61b0854d89587ea300eabd248d37c38d731517a
Ranulfo Mascari Neto <ranulfomascari@hotmail.com>
2023-08-08 21:19:52 -0300

    Email HTML

--- src/main/java/com/ranulfoneto/cursomc/services/EmailService.java
+++ src/main/java/com/ranulfoneto/cursomc/services/EmailService.java
@@ -4,9 +4,16 @@ import org.springframework.mail.SimpleMailMessage;
 
 import com.ranulfoneto.cursomc.domain.Pedido;
 
+import jakarta.mail.internet.MimeMessage;
+
 public interface EmailService {
     
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
+    
 }
